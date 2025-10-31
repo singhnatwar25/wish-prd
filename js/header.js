90503de9dd68aff8b3ac603205faca7f81c992a1
Natwar singh <127326133+singhnatwar25@users.noreply.github.com>
2025-10-31 16:42:35 +0530

    change wish-prd

--- js/header.js
+++ js/header.js
@@ -59,20 +59,20 @@ document.addEventListener("DOMContentLoaded", function () {
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="pricing.html" id="plansDropdown" role="button"
                 data-bs-toggle="dropdown" aria-expanded="false"
-                onclick="window.location.href = '/wish-prd/pricing.html';">Plans</a>
+                onclick="window.location.href = './pricing.html';">Plans</a>
               <ul class="dropdown-menu" aria-labelledby="plansDropdown">
                 <li><a class="dropdown-header">Dedicated Workspaces</a></li>
-                <li><a class="dropdown-item" href="/wish-prd/plan/private-office.html">Private Office</a></li>
-                <li><a class="dropdown-item" href="/wish-prd/plan/dedicated.html">Dedicated Desk</a></li>
-                <li><a class="dropdown-item" href="/wish-prd/plan/enterprise.html">Enterprises solution</a></li>
-                <li>
+                <li><a class="dropdown-item" href="/plan/private-office.html">Private Office</a></li>
+                <li><a class="dropdown-item" href="/plan/dedicated.html">Dedicated Desk</a></li>
+                <li><a class="dropdown-item" href="/plan/enterprise.html">Enterprises solution</a></li>
+                <li>  
                   <hr class="dropdown-divider">
                 </li>
                 <li><a class="dropdown-header fw-light ">Flexible Workspaces</a></li>
-                <li><a class="dropdown-item" href="/wish-prd/plan/coworking.html">Coworking</a></li>
-                <li><a class="dropdown-item" href="/wish-prd/plan/meeting.html">Meeting Room</a></li>
-                <li><a class="dropdown-item" href="/wish-prd/plan/virtual-office.html">Virtual Office</a></li>
-                <li><a class="dropdown-item" href="/wish-prd/plan/on-the-go.html">On-the-Go</a></li>
+                <li><a class="dropdown-item" href="/plan/coworking.html">Coworking</a></li>
+                <li><a class="dropdown-item" href="/plan/meeting.html">Meeting Room</a></li>
+                <li><a class="dropdown-item" href="/plan/virtual-office.html">Virtual Office</a></li>
+                <li><a class="dropdown-item" href="/plan/on-the-go.html">On-the-Go</a></li>
               </ul>
             </li>
             <li class="nav-item dropdown">
